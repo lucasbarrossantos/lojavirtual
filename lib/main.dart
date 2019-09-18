19ae73f7b0ca87521fe874b6aa696c20679d1c90
Lucas Barros <lucas-barros28@hotmail.com>
2019-09-18 20:32:54 -0300

    Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/screens/home_screen.dart';
+import 'package:lojavirtual/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color.fromARGB(255, 4, 125, 141)
       ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen()//HomeScreen(),
     );
   }
 }
