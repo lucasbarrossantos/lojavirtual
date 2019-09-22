36a78ac8db8f6906a8e3ce4703b99f234a10a5d0
Lucas Barros <lucas-barros28@hotmail.com>
2019-09-21 22:31:25 -0300

    Endereços das lojas

--- lib/tabs/orders_tab.dart
+++ lib/tabs/orders_tab.dart
@@ -22,7 +22,10 @@ class OrdersTab extends StatelessWidget {
           else
             return ListView(
               children: snapshot.data.documents
-                  .map((doc) => OrderTile(doc.documentID)).toList(),
+                  .map((doc) => OrderTile(doc.documentID))
+                  .toList()
+                  .reversed
+                  .toList(),
             );
         },
       );
